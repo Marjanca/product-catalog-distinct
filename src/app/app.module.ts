64160a832a2466505088dca582c3063cdabcc750
ADS\mapo <Marija.Popovic@zuehlke.com>
2017-07-05 16:31:15 +0200

    basic packaged for previous added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductService } from './product-service';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    ProductListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+          { path: 'home', component: HomeComponent },
+          { path: 'product-list', component: ProductListComponent },
+          { path: '**', redirectTo: 'home' }
+      ])
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
